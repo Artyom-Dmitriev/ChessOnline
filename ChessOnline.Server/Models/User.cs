d95db790e96203d9d2d5093fdff4ae9684d67f40
Artyom-Dmitriev <artemdmitriev06@gmail.com>
2026-04-06 21:58:54 +0400

    Добавлен User.cs в папку Models, созданы поля класса User

---
+++ ChessOnline.Server/Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace ChessOnline.Server.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Nickname { get; set; }
+        public string? Email { get; set; }
+        public string? PasswordHash { get; set; }
+        public bool IsGuest { get; set; }
+        public DateTime? GuestExpiresAt { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public int EloRating { get; set; } = 1200; // Default starting Elo rating
+    }
+}
