888644ca7f47bc9932fd7f5815215b45698d9859
Artyom-Dmitriev <artemdmitriev06@gmail.com>
2026-04-10 11:28:13 +0400

    добавлена модель Game, для модели Game добавлен enum GameStatus

---
+++ ChessOnline.Server/Models/Game.cs
@@ -0,0 +1,22 @@
+﻿namespace ChessOnline.Server.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public int WhitePlayerId { get; set; }
+        public int BlackPlayerId { get; set; }
+        public int TimeControlSeconds { get; set; } 
+        public int IncrementSeconds { get; set; } 
+        public DateTime? StartedAt { get; set; }
+        public DateTime? FinishedAt { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public int? WinnerId { get; set; }
+        public GameStatus Status { get; set; } = GameStatus.Waiting;
+
+        // Навигационные свойства
+
+        public User WhitePlayer { get; set; }
+        public User BlackPlayer { get; set; }
+        public User? Winner { get; set;}
+    }
+}
---
+++ ChessOnline.Server/Models/GameStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace ChessOnline.Server.Models
+{
+    public enum GameStatus
+    {
+        Waiting,
+        Active,
+        Finished,
+        Abandoned
+    }
+}
